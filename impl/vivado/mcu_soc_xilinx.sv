368a5a5a86a164b726e8248567240df9c93ae585
carotium <coolboy9987@gmail.com>
2026-04-29 11:58:22 +0000

    Added outputs for SPI and connected it to underlying mcu_soc module.

--- impl/vivado/mcu_soc_xilinx.sv
+++ impl/vivado/mcu_soc_xilinx.sv
@@ -3,20 +3,46 @@ module mcu_soc_xilinx import mcu_soc_pkg::*; #(
   parameter  int    INIT_FILE_BIN=0,
   parameter  int    MEM_SIZE_WORDS=4096
   ) (
-  input  logic clk,
-  input  logic rst,
+  input logic diff_clk_p,
+  input logic diff_clk_n,
+//  input  logic clk,
+  input  logic rstn,
 
   output logic tx,
-  input  logic rx
+  input  logic rx,
+
+  output logic [3:0] ss,
+  output logic sclk,
+  input logic miso,
+  output logic mosi,
+  output logic complete
 );
+
+  logic clk;
+
+  IBUFGDS #(
+    .DIFF_TERM("FALSE"),
+    .IBUF_LOW_PWR("TRUE"),
+    .IOSTANDARD("DEFAULT")
+    ) IBUFGDS_inst (
+       .O(clk),
+        .I(diff_clk_p),
+        .IB(diff_clk_n)
+        );
+
   mcu_soc #(
     .INIT_FILE     (INIT_FILE),
     .INIT_FILE_BIN (INIT_FILE_BIN),
     .MEM_SIZE_WORDS(MEM_SIZE_WORDS)
   ) mcu1 (
-    .clk (clk),
-    .rstn(~rst),
-    .tx  (tx),
-    .rx  (rx)
+    .clk  (clk),
+    .rstn (rstn),
+    .tx   (tx),
+    .rx   (rx),
+    .ss	  (ss),
+    .sclk (sclk),
+    .mosi (mosi),
+    .miso (miso),
+    .complete (complete)
   );
 endmodule
