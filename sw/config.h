30a9cf8f42f80d5bbda1b73431c9a89e273b7512
carotium <coolboy9987@gmail.com>
2026-04-29 12:05:07 +0000

    Changed frequency to 200 MHz (genesys2). Added defines for SPI frequency and SPI base address.

--- sw/config.h
+++ sw/config.h
@@ -12,9 +12,10 @@
 // Address map
 #define BOOTROM_BASE_ADDR   0x80000000
 #define UART_BASE_ADDR      0x60000000
+#define SPI_BASE_ADDR 0x40000000
 
 // Frequencies
-#define TB_FREQUENCY        100000000
+#define TB_FREQUENCY        200000000
 #define TB_BAUDRATE         115200
 
 // Peripheral configs
@@ -22,3 +23,7 @@
 #define UART_BYTE_ALIGN     4
 #define UART_FREQ           TB_FREQUENCY
 #define UART_BAUD           TB_BAUDRATE
+
+// SPI
+#define SPI_FREQ	    10000000
+#define CLK_FREQ	    TB_FREQUENCY
